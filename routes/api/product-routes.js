0f461b593667ae353f4ce7354055849f51ab9827
erkanoktenn <erkannoktenn@gmail.com>
2024-08-16 14:26:13 -0400

    Add API routes for categories, products, and tags

---
+++ routes/api/product-routes.js
@@ -0,0 +1,79 @@
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const productData = await Product.create(req.body);
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const productData = await Product.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData[0]) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with this id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
